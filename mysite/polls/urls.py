1dfc4a11e66b113cae4b04813b71633056c751d0
Tanish Garg <tanishgarg96@gmail.com>
2018-04-11 17:59:57 +0530

    -- vote not working- generic view
    result/detail working,  Changed view to generic, problem finding in url.py & related for !!!>NoReverseMatch at /1/<!!! error.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,15 @@ from . import views
 
 app_name= 'polls'
 urlpatterns = [
+    url(r'^$',views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'), #<int:question_id>
+    url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(), name='results'), #<int:question_id>/results/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'), #<int:question_id>/vote/
+]
+'''urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$',views.detail, name='detail'), #<int:question_id>
     url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'), #<int:question_id>/results/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'), #<int:question_id>/vote/
 ]
+'''
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,9 +7,10 @@ from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 #Create Views here
-def index(request):
+'''def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #output = ', '.join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
@@ -27,16 +28,31 @@ def results(request, question_id):
     #return HttpResponse("you're looking at results of question %s." % question_id)
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/result.html', {'question':question})
+'''
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name= 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     #return HttpResponse("you're voting the question %s." % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set_get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, choice.DoesNotExist):
         return render(request, 'polls/detail.html',{
-            'question':question,
-            'error_message':"You didn't selected"
+            'question': question,
+            'error_message': "You didn't selected",
         })
     else:
         selected_choice.votes +=1
